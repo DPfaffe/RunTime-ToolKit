1f78787f9ac60a878f6ad10c0f68d1415e9c33a8
SwiftExpat <76655220+SwiftExpat@users.noreply.github.com>
2022-10-01 17:20:50 -0500

    renamed methods

--- Samples/FNCChart/WebinarFMX/frmFncChartGridFMXU.pas
+++ Samples/FNCChart/WebinarFMX/frmFncChartGridFMXU.pas
@@ -41,9 +41,8 @@ type
     procedure FDConnectionAfterConnect(Sender: TObject);
   private
     FQuery: TFDQuery;
-    procedure CopyDataSet;
-    procedure DataAppend2019;
-    procedure DataAppend2020;
+    procedure ChartDataETL;
+    procedure DataAppend(AYear, ABaseAmount: integer);
   public
     { Public declarations }
   end;
@@ -53,55 +52,39 @@ var
 
 implementation
 
-
 {$R *.fmx}
 
-procedure TfrmChartSalesFMX.CopyDataSet;
+procedure TfrmChartSalesFMX.ChartDataETL;
 begin
   FQuery := TFDQuery.Create(self);
   FQuery.Connection := FDConnection;
   FQuery.Open('Select * from SalesInfo order by year desc, month');
-  DataAppend2019;
-  fdqSalesChart.Active := true;     //THIS LINE NEEDS TO MOVE AFTER APPEND 2020
-  DataAppend2020;
+  DataAppend(2019, 125);
+  fdqSalesChart.Active := true; // THIS LINE NEEDS TO MOVE AFTER APPEND 2020
+  DataAppend(2020, 150);
   FQuery.Active := false;
-  FQuery.Active := true; //re-fectch data with the order by
+  FQuery.Active := true; // re-fectch data with the order by
   FDMemTableSalesGrid.CopyDataSet(FQuery, [coStructure, coRestart, coAppend]);
-  FDQSalesPie.Active := true;
+  fdqSalesPie.Active := true;
 end;
 
-procedure TfrmChartSalesFMX.DataAppend2019;
+procedure TfrmChartSalesFMX.DataAppend(AYear, ABaseAmount: integer);
 var
   i: integer;
 begin
   for i := 1 to 12 do
   begin
     FQuery.Append;
-    FQuery.Fields[0].AsInteger := 2019;
+    FQuery.Fields[0].AsInteger := AYear;
     FQuery.Fields[1].AsInteger := i;
-    FQuery.Fields[2].AsInteger := i * 125;
-    FQuery.Post;
-  end;
-end;
-
-procedure TfrmChartSalesFMX.DataAppend2020;
-var
-  i: integer;
-begin
-  for i := 1 to 12 do
-  begin
-    FQuery.Append;
-    FQuery.Fields[0].AsInteger := 2020;
-    FQuery.Fields[1].AsInteger := i;
-    FQuery.Fields[2].AsInteger := i * 150;
+    FQuery.Fields[2].AsInteger := i * ABaseAmount;
     FQuery.Post;
   end;
 end;
 
 procedure TfrmChartSalesFMX.FDConnectionAfterConnect(Sender: TObject);
 begin
- FDConnection.ExecSQL
-    ('CREATE TABLE Chart_Months (Month INT NOT NULL, DisplayName Varchar);');
+  FDConnection.ExecSQL('CREATE TABLE Chart_Months (Month INT NOT NULL, DisplayName Varchar);');
   FDConnection.ExecSQL
     ('INSERT INTO Chart_Months(Month, DisplayName) VALUES(1, ''Jan''), (2, ''Feb''), (3, ''Mar''), (4, ''Apr''), (5, ''May''), (6, ''Jun''), (7, ''Jul''), (8, ''Aug''), (9, ''Sept''), (10, ''Oct''), (11, ''Nov''), (12, ''Dec'');');
   FDConnection.ExecSQL
@@ -117,14 +100,14 @@ begin
   FDConnection.ExecSQL('CREATE VIEW [Sales_2020] as SELECT month, amount, year FROM SalesInfo where year = 2020;');
   FDConnection.ExecSQL
     ('CREATE VIEW [Sales_Chart] as  Select m.month, m.displayName as mdisp, s1.amount as SA2018, s2.amount as SA2019, s3.amount as SA2020 from Chart_Months m left join '
-    +' sales_2018 s1 on m.month = s1.month left join sales_2019 s2 on m.month = s2.month left join sales_2020 s3 on m.month = s3.month;');
+    + ' sales_2018 s1 on m.month = s1.month left join sales_2019 s2 on m.month = s2.month left join sales_2020 s3 on m.month = s3.month;');
 
 end;
 
 procedure TfrmChartSalesFMX.FormCreate(Sender: TObject);
 begin
   FDConnection.Open();
-  CopyDataSet;
+  ChartDataETL;
   TMSFNCGridDatabaseAdapter1.Active := true;
   TMSFNCChartDatabaseAdapter1.Active := true;
   TMSFNCChartDatabaseAdapter2.Active := true;
