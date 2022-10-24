72f848ce4f1f3b1cc204ee6e73c9d52da1d331ae
SwiftExpat <76655220+SwiftExpat@users.noreply.github.com>
2022-10-24 18:48:17 -0500

    Update chart inspector

--- Samples/FNCChart/Autonomous_Diagnostics_3-of-3.md
+++ Samples/FNCChart/Autonomous_Diagnostics_3-of-3.md
@@ -22,7 +22,12 @@ You save a copy of the chart to PDF just to ensure you have enough details to do
 
 Marshal internal inspectors are written using a plugin model, allowing you to deliver capabilites based on your component type. The FNC Chart Editor shown in this demo is a custom inspector for the TTMSFNCChart type. Source code is available in GitHub.
 
-TMS FNC Chart is a complex component which has a dedicated editor for design. Having the same editor available at runtime is more efficent than using Object Plus to inspect deeply nested properties. The custom inspector allows you to use the best tool for the component and still have Object Plus for inspecting other properties.
+TMS FNC Chart is a complex component which has a dedicated editor for design. Having the same editor available at runtime is more efficent than using Object Plus to inspect deeply nested properties. The custom inspector allows you to use the best tool for the component and still have Object Plus for inspecting other properties. The inspector provides higher level features:
+
+* FNC Chart Editor. Dedicated design tool for FNC Charts.
+* Export the chart to PDF. PDF is a good way to capture and share an image of the individual component. FNC Core provides TTMSFNCGraphicsPDFIO that automatically caputres the component.
+* Chart Image cache. With each change the image of the chart is captured and displayed in the list.
+* Adapter verification. The status bar demonstrates how automated checks can be used to provide a quick visual of common mis-configurations. Verifying all of the adapter dependencies is peformed using an nested if statement to ensure nothing is missed.
 
 Implementing a custom inspector involves 4 steps:
 
@@ -31,9 +36,4 @@ Implementing a custom inspector involves 4 steps:
 3. Create a inspector instance to allow the UI to initialize a the tool when you click on the node in the tree (TSERTTKPluginFNCChartTool)
 4. Initialize the inspector to add it to Marshal's registry
 
-The instance for this inspector is a frame, allowing use of the components from the tool pallet, that exposes a few public procedures for the buttons and a few UI components to visualize the result of the editor. The frame unit also contains the transform and tool mapping classes. The inspector provides higher level features:
-
-* FNC Chart Editor. Dedicated design tool for FNC Charts.
-* Export the chart to PDF. PDF is a good way to capture and share an image of the individual component. FNC Core provides TTMSFNCGraphicsPDFIO that automatically caputres the component.
-* Chart Image cache. With each change the image of the chart is captured and displayed in the list.
-* Adapter verification. The status bar demonstrates how automated checks can be used to provide a quick visual of common mis-configurations. Verifying all of the adapter dependencies is peformed using an nested if statement to ensure nothing is missed.
+The instance for this inspector is a frame, allowing use of the components from the tool pallet, that exposes a few public procedures for the buttons and a few UI components to visualize the result of the editor. The frame unit also contains the transform and tool mapping classes.
--- Samples/FNCChart/Inspectors/FMX.RTTK.PT.FNCChartTool.pas
+++ Samples/FNCChart/Inspectors/FMX.RTTK.PT.FNCChartTool.pas
@@ -23,34 +23,71 @@ type
     procedure TMSFNCListBox1ItemClick(Sender: TObject; AItem: TTMSFNCListBoxItem);
   private
     FChart: TTMSFNCChart; // used for references
+    /// <summary>Creates an entry in the listbox for an action</summary>
+    /// <remarks>assigns the image index and json string</remarks>
     procedure LogChartAction(const AMessage: string);
+    /// <summary>Callback for TTMSFNCUtils.SaveFile </summary>
+    /// <remarks>Executes the export if result was true</remarks>
     procedure ExportChartPDF(const AFile: string; const AResult: Boolean);
+    /// <summary>Captures bitmap image of chart component</summary>
+    /// <remarks>Display is in a FNC image component and reference is in fnc listbox</remarks>
+    /// <returns>Index of bitmap in the bitmap container</returns>
     function CaptureChartImage: integer;
+    /// <summary>Checks assignment of adapter, data source and dataset</summary>
+    /// <remarks>Displays as status bar panels</remarks>
     procedure StatusBarLoad;
   public
+    /// <summary>Calls the FNC Chart editor from button click event</summary>
+    /// <remarks>Exposed public for the button in the tool bar to access</remarks>
     procedure ShowEditor(Sender: TObject);
+    /// <summary>Exports the FNC Chart as pdf using file save and a pdf component</summary>
+    /// <remarks>Exposed public for the button in the tool bar to access</remarks>
     procedure ExportPDF(Sender: TObject);
+    /// <summary>Passes the chart reference from the tool instance</summary>
+    /// <remarks>Tool instance will check and cast the component to FNC Chart</remarks>
     procedure AssignChartToEditor(AChart: TTMSFNCChart);
   end;
 
+  /// <summary>Classtype mapping for the registry. What class type does this inspector work for?</summary>
+  /// <remarks>Regsitry controls disposal</remarks>
+  /// <returns>Instance in the registry via the initialization section</returns>
   TSETransformFNCChart = class(TSERTTKPluginTransform)
   protected
+    /// <summary>Assigns the tool instance to the component if class type matches</summary>
+    /// <remarks>Called for each component in the application</remarks>
     procedure ExecTransform; override;
   public
+    /// <summary>Assigns the classtype</summary>
+    /// <remarks> /\/\ </remarks>
     constructor Create; reintroduce;
   end;
 
+  /// <summary>Mapping class for transform to assign the inspector type to the component instance</summary>
+  /// <remarks>intermediate class to later build the inspector UI</remarks>
+  /// <returns>Reference for the component to later display inspectors</returns>
   TSESITFNCChart = class(TSERTTKPluginInspectTool)
   public
+    /// <summary>Factory method to allow implementation of custom inspector</summary>
+    /// <remarks>Called once the component tab has been created</remarks>
+    /// <returns>Inspector to be initialized</returns>
     function InitToolUI(const AInspectorCVInfo: TSERTTKPluginInspectorCVInfo): TSERTTKPluginToolInstance; override;
   end;
 
+  /// <summary>Custom inspector for TMSFNCChart</summary>
+  /// <remarks>Creates the frame which implements inspector logic</remarks>
+  /// <returns>Frame is parented to the client area and align is client</returns>
   TSERTTKPluginFNCChartTool = class(TSERTTKPluginToolInstance)
   strict private
     FChartUITool: TFrameFNCChartTool;
   protected
+    /// <summary>Executes the initial inspector load procedures</summary>
+    /// <remarks>Called when the click in the nav tree happens, late binding</remarks>
+    /// <returns>The frame that implements the inspector functionality in the client area</returns>
     procedure InitToolInstance; override;
   public
+    /// <summary>Returns the display name for the inspector tab</summary>
+    /// <remarks> /\/\ </remarks>
+    /// <returns>String display name</returns>
     function DisplayName: string; override;
   end;
 
@@ -80,7 +117,7 @@ begin
   TMSFNCGraphicsPDFIO1.Options.Header := TMSFNCGraphicsPDFIO1.Information.Title;
   TMSFNCGraphicsPDFIO1.Options.Footer := 'Exported @ ' + FormatDateTime(fmt_datetime, now);
   if FChart.Width > FChart.height then
-    TMSFNCGraphicsPDFIO1.Options.PageOrientation  := TTMSFNCPDFlibPageOrientation.poLandscape;
+    TMSFNCGraphicsPDFIO1.Options.PageOrientation := TTMSFNCPDFlibPageOrientation.poLandscape;
   TMSFNCGraphicsPDFIO1.Save(AFile);
 end;
 
@@ -133,13 +170,13 @@ var
     result.Style := TTMSFNCStatusBarPanelStyle.spsHTML;
     result.AutoSize := true;
   end;
-  function TextGreen(AText:string):string;
+  function TextGreen(AText: string): string;
   begin
-    result := '<FONT color="#00FF00" >'+AText+'</FONT>';
+    result := '<FONT color="#008800" >' + AText + '</FONT>';
   end;
-  function TextRed(AText:string):string;
+  function TextRed(AText: string): string;
   begin
-    result := '<FONT color="#FF0000">'+AText+'</FONT>';
+    result := '<FONT color="#FF0000">' + AText + '</FONT>';
   end;
 
 begin
@@ -199,7 +236,7 @@ var
 begin
   try
     try
-    TMSFNCImage1.Bitmap := TMSFNCBitmapContainer1.Items[AItem.DataInteger].Bitmap;
+      TMSFNCImage1.Bitmap := TMSFNCBitmapContainer1.Items[AItem.DataInteger].Bitmap;
       JV := TJSONObject.ParseJSONValue(AItem.DataString);
       TMSFNCHTMLText1.Text := JV.Format(1);
     except
